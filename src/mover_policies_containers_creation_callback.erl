b267d5d51d6c41bb932b075b6b9748eaf4bfc141
Marc Paradise <marc@chef.io>
2015-05-08 14:23:06 -0400

    Add logging and use correct superuser authz id
    * log http request failures to set ace
    * correct using wrong superuser authzid

--- src/mover_policies_containers_creation_callback.erl
+++ src/mover_policies_containers_creation_callback.erl
@@ -71,7 +71,8 @@ migration_init() ->
     %% TODO: this fails if the pool is already created. Should be tolerant of
     %% errors and also should tear this down after the migration
     mv_oc_chef_authz_http:create_pool(),
-    SuperUserAuthzId = envy:get(oc_chef_authz,authz_superuser_id, binary),
+    {ok, SuperuserResults} = sqerl:adhoc_select([username, id, authz_id], users, {username, equals, <<"pivotal">>}),
+    {_, SuperUserAuthzId} =  proplists:lookup(<<"authz_id">>,  hd(SuperuserResults)),
     Superuser = #mover_requestor{authz_id = SuperUserAuthzId},
 
     FindOrgsWithNoPoliciesContainer = <<"SELECT orgs.name, orgs.id, orgs.authz_id FROM orgs "
